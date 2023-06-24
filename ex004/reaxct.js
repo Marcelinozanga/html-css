167f8091267060f025a787d6a838af8d968723dd
Marcelinozanga <137086962+Marcelinozanga@users.noreply.github.com>
2023-06-24 20:58:01 +0100

    Initial commit

---
+++ ex004/reaxct.js
@@ -0,0 +1,14 @@
+let colocacao = 3
+switch(colocacao){
+    case 1:
+        console.log('primeiro lugar')
+        break
+        case 2:
+            console.log('segundo lugar')
+                break
+                case 3:
+                    console.log('terceiro lugar')
+                    break
+
+
+}
